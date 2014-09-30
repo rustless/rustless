ea93721750e591fa8a3b46b3b2698cb1af1134ac
Stanislav Panferov <fnight.m@gmail.com>
2014-09-30 16:25:59 +0400

    style: convert tabs to spaces

--- src/raisin.rs
+++ src/raisin.rs
@@ -5,20 +5,20 @@ use listener::{Listener};
 use middleware::Application;
 
 pub struct Raisin {
-	pub some: u16,
+    pub some: u16,
 }
 
 impl Raisin {
 
-	pub fn listen(self, app: Application, ip: IpAddr, port: u16) {
-		spawn(proc() {
-			let arc_app = Arc::new(app);
-			Listener {
-				ip: ip,
-				port: port,
-				app: arc_app
-			}.serve();
-		})
-	}
+    pub fn listen(self, app: Application, ip: IpAddr, port: u16) {
+        spawn(proc() {
+            let arc_app = Arc::new(app);
+            Listener {
+                ip: ip,
+                port: port,
+                app: arc_app
+            }.serve();
+        })
+    }
 
 }
--- src/request.rs
+++ src/request.rs
@@ -9,8 +9,8 @@ use http::server::request::Request as HttpRequest;
 use anymap::AnyMap;
 
 pub struct Request {
-	pub url: Url,
-	pub remote_addr: Option<SocketAddr>,
+    pub url: Url,
+    pub remote_addr: Option<SocketAddr>,
     pub headers: HeaderCollection,
     pub body: String,
     pub method: Method,
@@ -20,46 +20,46 @@ pub struct Request {
 impl Show for Request {
     fn fmt(&self, f: &mut Formatter) -> Result<(), FormatError> {
         try!(writeln!(f, "Request ->"));
-        try!(writeln!(f, "	url: {}", self.url));
-        try!(writeln!(f, "	method: {}", self.method));
-        try!(writeln!(f, "	path: {}", self.url.path()));
-        try!(writeln!(f, "	query: {}", self.url.query));
-        try!(writeln!(f, "	remote_addr: {}", self.remote_addr));
-        try!(writeln!(f, "	body: {}", self.body));
+        try!(writeln!(f, "  url: {}", self.url));
+        try!(writeln!(f, "  method: {}", self.method));
+        try!(writeln!(f, "  path: {}", self.url.path()));
+        try!(writeln!(f, "  query: {}", self.url.query));
+        try!(writeln!(f, "  remote_addr: {}", self.remote_addr));
+        try!(writeln!(f, "  body: {}", self.body));
         Ok(())
     }
 }
 
 impl Request {
-	pub fn wrap(req: HttpRequest) -> Result<Request, String> {
-		
-		let url = match req.request_uri {
-			
-			AbsoluteUri(url) => format!("{}", url),
-			AbsolutePath(path) => {
-				match req.headers.host {
+    pub fn wrap(req: HttpRequest) -> Result<Request, String> {
+        
+        let url = match req.request_uri {
+            
+            AbsoluteUri(url) => format!("{}", url),
+            AbsolutePath(path) => {
+                match req.headers.host {
                     Some(ref host) => format!("http://{}{}", host, path),
                     None => return Err("No HOST header specified in request".to_string())
                 }
-			},
+            },
 
-			_ => return Err("Unsupported request URI".to_string())
+            _ => return Err("Unsupported request URI".to_string())
 
-		};
+        };
 
-		let parsed_url = match Url::parse(url.as_slice()) {
-			Ok(url) => url,
-			Err(parse_error) => return Err(format!("{}", parse_error))
-		};
+        let parsed_url = match Url::parse(url.as_slice()) {
+            Ok(url) => url,
+            Err(parse_error) => return Err(format!("{}", parse_error))
+        };
 
-		Ok(Request {
-			url: parsed_url,
-			remote_addr: req.remote_addr,
-			headers: req.headers,
-			body: req.body,
-			method: req.method,
-			ext: AnyMap::new()
-		})
+        Ok(Request {
+            url: parsed_url,
+            remote_addr: req.remote_addr,
+            headers: req.headers,
+            body: req.body,
+            method: req.method,
+            ext: AnyMap::new()
+        })
 
-	}
+    }
 }
