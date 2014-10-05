714df4caac4fc9616f1a938bf74e28e9579c4d34
Stanislav Panferov <fnight.m@gmail.com>
2014-10-05 20:53:45 +0400

    feat: now we can respond with json objects or simple text

--- src/api/endpoint.rs
+++ src/api/endpoint.rs
@@ -8,7 +8,10 @@ use std::str;
 use anymap::AnyMap;
 use hyper::method::{Method};
 use hyper::status;
+use hyper::mime;
 use hyper::header;
+use hyper::header::Header;
+use hyper::header::common::{ContentType, Location};
 use valico::Builder as ValicoBuilder;
 use query;
 
@@ -187,16 +190,42 @@ impl<'a> EndpointInstance<'a> {
         }
     }
 
+    pub fn set_status(&mut self, status: status::StatusCode) {
+        self.response.status = status;
+    }
+
+    pub fn set_header<H: Header>(&mut self, header: H) {
+        self.response.headers.set(header);
+    }
+
+    pub fn set_json_content_type(&mut self) {
+        let application_json: mime::Mime = from_str("application/json").unwrap();
+        self.set_header(ContentType(application_json));
+    }
+
+    pub fn json(mut self, result: &Json) -> EndpointInstance<'a> {
+        self.set_json_content_type();
+        self.response.push_string(result.to_string());
+
+        self
+    }
+
+    pub fn text(mut self, result: String) -> EndpointInstance<'a> {
+        self.response.push_string(result);
+
+        self
+    }
+
     pub fn redirect(mut self, to: &str) -> EndpointInstance<'a> {
-        self.response.status = status::Found;
-        self.response.headers.set(header::common::Location(to.to_string()));
+        self.set_status(status::Found);
+        self.set_header(Location(to.to_string()));
 
         self
     }
 
     pub fn permanent_redirect(mut self, to: &str) -> EndpointInstance<'a> {
-        self.response.status = status::MovedPermanently;
-        self.response.headers.set(header::common::Location(to.to_string()));
+        self.set_status(status::MovedPermanently);
+        self.set_header(Location(to.to_string()));
 
         self
     }
