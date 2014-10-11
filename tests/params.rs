c10d7dc028bb4c2ce5e92eac396b9da9c06dc562
Stanislav Panferov <fnight.m@gmail.com>
2014-10-11 13:22:22 +0400

    feat: move params parsing to Api stage to allow namespace query and body params validation

--- tests/params.rs
+++ tests/params.rs
@@ -1,4 +1,5 @@
 use url::Url;
+use std::str;
 use serialize::json::{JsonObject};
 use rustless::server_backend::method::{Get};
 use rustless::server_backend::status;
@@ -58,4 +59,76 @@ fn it_validates_typed_endpoint_path_params() {
     let response = call_app!(app, Get, "http://127.0.0.1:3000/api/users/100/messages/12").unwrap();
     assert_eq!(response.status, status::Ok);
 
+}
+
+#[test]
+fn it_validates_query_params() {
+
+    let app = app!(|api| {
+        api.prefix("api");
+
+        api.get("users/:user_id", |endpoint| {
+            endpoint.params(|params| {
+                params.req_typed("user_id", Valico::u64());
+                params.req("profile", |profile| {
+                    profile.allow_values(["simple".to_string(), "full".to_string()])
+                })
+            });
+
+            edp_stub_handler!(endpoint)
+        })
+    });
+
+    let response = call_app!(app, Get, "http://127.0.0.1:3000/api/users/100").unwrap();
+    assert_eq!(response.status, status::BadRequest);    
+
+    let response = call_app!(app, Get, "http://127.0.0.1:3000/api/users/100?profile=1").unwrap();
+    assert_eq!(response.status, status::BadRequest);    
+
+    let response = call_app!(app, Get, "http://127.0.0.1:3000/api/users/100?profile=fulll").unwrap();
+    assert_eq!(response.status, status::BadRequest);        
+
+    let response = call_app!(app, Get, "http://127.0.0.1:3000/api/users/100?profile=full").unwrap();
+    assert_eq!(response.status, status::Ok);    
+
+    let response = call_app!(app, Get, "http://127.0.0.1:3000/api/users/100?profile=simple").unwrap();
+    assert_eq!(response.status, status::Ok);   
+     
+}
+
+#[test]
+fn it_validates_common_namespace_params() {
+
+    let app = app!(|api| {
+        api.prefix("api");
+
+        api.resources("users/:user_id", |users| {
+            users.params(|params| {
+                // one parameter goes from path and one from query-string or body
+                params.req_typed("user_id", Valico::u64());
+                params.req_typed("ext", Valico::string());
+            });
+
+            users.get("profile/:profile", |endpoint| {
+                endpoint.params(|params| {
+                    params.req("profile", |profile| {
+                        profile.allow_values(["simple".to_string(), "full".to_string()])
+                    })
+                });
+
+                edp_stub_handler!(endpoint)
+            })
+        })
+    });
+
+    let response = call_app!(app, Get, "http://127.0.0.1:3000/api/users/100").unwrap();
+    assert_eq!(response.status, status::BadRequest);    
+
+    let response = call_app!(app, Get, "http://127.0.0.1:3000/api/users/100/profile/full").unwrap();
+    // missed `ext` param
+    assert_eq!(response.status, status::BadRequest);  
+
+    let mut response = call_app!(app, Get, "http://127.0.0.1:3000/api/users/100/profile/full?ext=some").unwrap();
+        println!("{}", resp_body!(response));
+    assert_eq!(response.status, status::Ok);   
 }
