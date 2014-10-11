5dc12ba26dcd6036511fee74605742a5e33bc97b
Stanislav Panferov <fnight.m@gmail.com>
2014-10-11 16:10:53 +0400

    Trivial usage of media.format in default validation_error_formatter.

--- src/framework/formatters.rs
+++ src/framework/formatters.rs
@@ -2,15 +2,19 @@
 use serialize::json::ToJson;
 
 use errors::{Error, ErrorRefExt, ValidationError};
+use framework::media;
 use framework::media::Media;
 use server::Response;
 use server_backend::status::BadRequest;
 
-pub fn validation_error_formatter(err: &Box<Error>, _media: &Media) -> Option<Response> {
+pub fn validation_error_formatter(err: &Box<Error>, media: &Media) -> Option<Response> {
     match err.downcast::<ValidationError>() {
         Some(err) => {
-             // TODO respond with media
-            Some(Response::from_json(BadRequest, &err.reason.to_json()))   
+            match media.format {
+                media::JsonFormat => Some(Response::from_json(BadRequest, &err.reason.to_json())),
+                // TODO: Make formatter for a mere mortals
+                _ => Some(Response::from_string(BadRequest, err.reason.to_json().to_pretty_str()))  
+            }
         },
         None => None
     }
