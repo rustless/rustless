6a397152b92d0e7873cfae827785744a51c63a5e
Stanislav Panferov <fnight.m@gmail.com>
2015-01-20 11:30:15 +0300

    refactor(formatters): adjust return type and split lines

--- src/framework/formatters.rs
+++ src/framework/formatters.rs
@@ -2,20 +2,26 @@
 use serialize::json::{ToJson, as_pretty_json};
 
 use errors::{ValidationError};
+use framework::{ErrorFormatter};
 use framework::media;
 use framework::media::Media;
 use backend::{Response};
 use server::status::StatusCode::BadRequest;
 
 
-pub fn create_default_error_formatter() -> Box<Fn(&Box<::errors::Error + 'static>, &Media) -> Option<Response> + 'static + Send+Sync>  {
+pub fn create_default_error_formatter() -> ErrorFormatter  {
     let closure = |&: err: &Box<::errors::Error + 'static>, media: &Media| -> Option<Response> {
         match err.downcast::<ValidationError>() {
             Some(err) => {
                 match media.format {
                     media::Format::JsonFormat => Some(Response::from_json(BadRequest, &err.reason.to_json())),
                     // TODO: Make formatter for a mere mortals
-                    _ => Some(Response::from_string(BadRequest, as_pretty_json(&err.reason.to_json()).indent(4u32).to_string()))  
+                    _ => Some(
+                        Response::from_string(
+                            BadRequest, 
+                            as_pretty_json(&err.reason.to_json()).indent(4u32).to_string()
+                        )
+                    )  
                 }
             },
             None => None
