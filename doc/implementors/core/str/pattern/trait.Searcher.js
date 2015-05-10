5ee034d1f3eefdb3e74d7b98f156adc311bbc16b
Stanislav Panferov <fnight.m@gmail.com>
2015-05-10 08:40:31 +0000

    rebuild pages at 367b31c19cb5332226b8226f0fcbbec7043531bf

--- doc/implementors/core/str/pattern/trait.Searcher.js
+++ doc/implementors/core/str/pattern/trait.Searcher.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['openssl'] = [];implementors['hyper'] = [];implementors['iron'] = [];implementors['rustless'] = [];
+implementors['openssl'] = [];implementors['queryst'] = ["impl&lt;'r, 't&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/str/pattern/trait.Searcher.html' title='core::str::pattern::Searcher'>Searcher</a>&lt;'t&gt; for <a class='struct' href='regex/re/struct.RegexSearcher.html' title='regex::re::RegexSearcher'>RegexSearcher</a>&lt;'r, 't&gt;",];implementors['hyper'] = [];implementors['iron'] = [];implementors['rustless'] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
